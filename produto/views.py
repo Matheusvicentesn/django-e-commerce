3d72d5d846ef29cf1012ee61b9ba7375aecf34d0
Matheus Vicente <zero@pop-os.localdomain>
2022-03-15 19:27:10 -0300

    Construindo carrinho

--- produto/views.py
+++ produto/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
+
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -21,6 +23,32 @@ class DetalheProduto(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('produto:lista'))
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto inexistente'
+            )
+            return redirect(http_referer)
+
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session['cart']
+
+        if variacao_id in cart:
+            pass
+        else:
+            pass
+
+
         return HttpResponse('AddToCart')
 
 
@@ -32,6 +60,7 @@ class Remove(View):
 class Cart(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Cart')
+
 
 class Finish(View):
     def get(self, *args, **kwargs):
