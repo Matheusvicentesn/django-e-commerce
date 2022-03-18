345f4074004ab3451d21ad1ecbd5d7d865cd25a5
Matheus Vicente <zero@pop-os.localdomain>
2022-03-18 16:10:02 -0300

    Tentando dar deploy aws

---
+++ local.py
@@ -0,0 +1,1 @@
+key = 'django-insecure-kgl^mcpxh^#6%ncg&_itqwh2!o2_g%=pz+#xi!#^i3)ndcp72h'
--- loja/settings.py
+++ loja/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants
+from local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgl^mcpxh^#6%ncg&_itqwh2!o2_g%=pz+#xi!#^i3)ndcp72h'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
